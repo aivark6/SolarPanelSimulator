7ae65a5a201593a008d1e44ab36a5c910698b65b
aivark6 <aivar.koodi@gmail.com>
2016-03-26 21:33:10 +0200

    Working code upload
    Changed code is working

--- CubeSatMSP430Test/main.c
+++ CubeSatMSP430Test/main.c
@@ -4,177 +4,136 @@
 
 #include <msp430.h>
 
-#include <stdio.h>
 /***************************************************************************************************
  *	        Prototype section					                       							   *
  ***************************************************************************************************/
 
-//sensor data
-void send_systemdata(int data);
-void dac_write(char data);
 unsigned int measure_adc(void);
-signed int ResistanceToVoltage(int Resistance);
-signed int ResistanceToCurrent(int Resistance);
-int calculate_voltage(unsigned int adc_voltage_value);
-int calculate_current(unsigned int adc_voltage_value);
+unsigned int measuring_data(void);
+void send_data(unsigned int measured_resistance);
 
 /***************************************************************************************************
  *	        Global Variable section  				                            				   *
  ***************************************************************************************************/
 
-//Flags
-int send_data = 0;
+unsigned int recieved_voltage;
+unsigned int recieved_current;
 
-//Counter
-int counter = 0;
+unsigned int voltage;
+unsigned int current;
 
-//Sensors
-int model_output = 0;
-unsigned int adc_voltage_value = 0;
-unsigned int adc_current_value = 0;
+unsigned int processed_current_data[10];
+unsigned int processed_voltage_data;
 
-//DAC
-char sin[32] = {0x80, 0x99, 0xB2, 0xC8,//32 point sin wave sampled via MATLAB
- 0xDC, 0xEC, 0xF7, 0xFE,
- 0xFF, 0xFB, 0xF2, 0xE4,
- 0xD3, 0xBD, 0xA6, 0x8C,
- 0x73, 0x59, 0x42, 0x2C,
- 0x1B, 0x0D, 0x04, 0x00,
- 0x01, 0x08, 0x13, 0x23,
- 0x37, 0x4D, 0x66, 0x7F};
+unsigned int recieved_voltage;
+unsigned int recieved_current;
+
+unsigned int measured_voltage;
+unsigned int measured_current;
+
+unsigned int measured_resistance;
+unsigned int calculated_resistance;
+
+unsigned int dac_A_status = 12288;
+unsigned int dac_B_status = 45056;
+unsigned int sum;
+unsigned int voltage_msb;
+unsigned int voltage_lsb;
+unsigned int current_msb;
+unsigned int current_lsb;
+
+unsigned int data_recieved;
+
+char data_number = 0;
+
 
 /***************************************************************************************************
  *	        Define section					                       		   					       *
  ***************************************************************************************************/
-#define LED_0 BIT0
-#define LED_OUT P1OUT
-#define LED_DIR P1DIR
+
 
 /***************************************************************************************************
  *         Main section                                                                            *
  ***************************************************************************************************/
 
-void main(void)
-	{
-	//Stops Watchdog
-	WDTCTL = WDTPW | WDTHOLD;
+int main(void) {
+    WDTCTL = WDTPW | WDTHOLD;	// Stop watchdog timer
 
-	//Calibrate timer
-	if(CALBC1_16MHZ==0xFF)
+	//Calibrate timer 1MHZ
+	if(CALBC1_1MHZ==0xFF)
 	{
 		while(1);
 	}
 	DCOCTL = 0;
-	BCSCTL1 = CALBC1_16MHZ;
-	DCOCTL = CALDCO_16MHZ;
+	BCSCTL1 = CALBC1_1MHZ;
+	DCOCTL = CALDCO_1MHZ;
 
-	//Timer 0
-	TA0CCTL0 = CCIE;
-	TA0CCR0 = 32768;
-	TA0CTL = TASSEL_1 + MC_1;
+    P1SEL  = BIT1 + BIT2 + BIT5 + BIT7;	 	//Enable module function on pin
+    P1SEL2 = BIT1 + BIT2 + BIT5 + BIT7;  	//Select second module; P1.1 = RXD, P1.2=TXD
 
-    //Enable red LED
-    LED_DIR = LED_0; //LEDs are set as output
-    LED_OUT &= ~(LED_0); //LEDs are turned off
+	P1DIR |= (BIT0 + BIT6);
+	P1OUT &= ~ (BIT0 + BIT6);
 
-//    //Enable ports for UART connection
-//	P1SEL  |= BIT1 + BIT2;	 	//Enable module function on pin
-//	P1SEL2 |= BIT1 + BIT2;  	//Select second module; P1.1 = RXD, P1.2=TXD
+	//For incoming UART
+    UCA0CTL1 |= UCSSEL_2;    	// Set clock source SMCLK
+    UCA0BR0 = 104;           	// 1MHz 9600 (N=BRCLK/baudrate)
+    UCA0BR1 = 0;             	// 1MHz 9600
+    UCA0MCTL = UCBRS0;       	// Modulation UCBRSx = 1
+    UCA0CTL1 &= ~UCSWRST;    	// **Initialize USCI state machine**
 
-	//Enable clock
-	UCA0CTL1 |= UCSSEL_2;    	// Set clock source SMCLK
-	UCA0BR0 = 104;           	// 1MHz 9600 (N=BRCLK/baudrate)
-	UCA0BR1 = 0;             	// 1MHz 9600
-	UCA0MCTL = UCBRS0;       	// Modulation UCBRSx = 1
-	UCA0CTL1 &= ~UCSWRST;    	// **Initialize USCI state machine**
+    //For outgoing SPI
+	UCB0CTL1   =   UCSWRST;
+	UCB0CTL0   |=  UCCKPH  +   UCMSB   +   UCMST   +   UCSYNC; //  3-pin,  8-bit   SPI master
+	UCB0CTL1   |=  UCSSEL_2;   //  SMCLK
+	UCB0BR0    |=  0;   //  /2
+	UCB0BR1    =   0;  //
+	UCB0CTL1   &=  ~UCSWRST;   //  **Initialize    USCIstate   machine**
 
-//	IE2 |= UCA0RXIE;         	//enable RX interrupt
+    IE2 |= UCA0RXIE;         	//enable RX interrupt
 
-	__enable_interrupt();
+    __enable_interrupt(); 		//enable interrupt globally
 
-	while(42)
-	{
+	while(1){
 		__bis_SR_register(GIE);
-
-		if(send_data)
-		{
-//			//start bit (255)
-//			model_output = 0xFF;
-//			send_systemdata((int) (model_output));
-
-			//temperature value (0..40)
-			ADC10CTL0 = ADC10SHT_2 + ADC10ON; //64 clocks for sample
-			ADC10CTL1 = INCH_4;	// Temp Sensor is at channel
-			adc_voltage_value = measure_adc();
-			model_output = calculate_voltage(adc_voltage_value);
-//			send_systemdata((int) (model_output));
-//saab MSC funktsiooni kasutada et s‰‰sta ressursi
-			ADC10CTL0 = ADC10SHT_2 + ADC10ON; //64 clocks for sample
-			ADC10CTL1 = INCH_5;	// Temp Sensor is at channel
-			adc_current_value = measure_adc();
-			model_output = calculate_current(adc_current_value);
-//			send_systemdata((int) (model_output));
-
-			for (x = 0; x < 32; x++)
-			dac_write(sin[x]);
-
-			P1OUT ^= LED_0;
-			//exit send_data
-			send_data = 0;
-		}
+		__delay_cycles(1000);
+		measured_resistance = measuring_data();
+		__delay_cycles(1000);
+		send_data(measured_resistance);
 	}
 }
 
-//#include <msp430x22x2.h>
-//#include <UF_DAC.h>
-//char sin[32] = {0x80, 0x99, 0xB2, 0xC8,//32 point sin wave sampled via MATLAB
-// 0xDC, 0xEC, 0xF7, 0xFE,
-// 0xFF, 0xFB, 0xF2, 0xE4,
-// 0xD3, 0xBD, 0xA6, 0x8C,
-// 0x73, 0x59, 0x42, 0x2C,
-// 0x1B, 0x0D, 0x04, 0x00,
-// 0x01, 0x08, 0x13, 0x23,
-// 0x37, 0x4D, 0x66, 0x7F};
-//void main(void){
-//char x;
-//WDTCTL = WDTPW + WDTHOLD;
-//DCOCTL = CALDCO_16MHZ;
-//BCSCTL1 = CALBC1_16MHZ;
-//while(42){
-//for (x = 0; x < 32; x++){
-//dac_write(sin[x]);
-//}
-//}
-//}
+void send_data(unsigned int measured_resistance){
 
-/* END: main */
+	voltage = processed_voltage_data;
+	current = processed_current_data[measured_resistance];
 
-void dac_write(char data){
-int output = 0x0000;
-char temp;
-signed char x;
-P2SEL &= 0xF8; //select digital I/0
-P2DIR |= 0x07; //set directon register P2.0 - P2.2
-P2OUT &= 0xF8; // clear data, clock, and enable
-P2OUT |= 0x04; // set enable high
-temp = data; // format data
-data >>= 4;
-data &= 0x0F;
-data |= 0xF0;
-output = data;
-output <<= 8;
-data = temp;
-data <<= 4;
-data &= 0xF0;
-output |= data;
-P2OUT &= 0xFB; //set enable low
-for (x = 15; x > -1; x--){ //send out 16 bits of data
-P2OUT |= (output >> x) & 0x01;
-P2OUT |= 0x02;
-P2OUT &= 0xFD;
-P2OUT &= 0xFE;
-}
-P2OUT |= 0x04; // set enable high
+	sum = dac_A_status + voltage;
+
+	voltage_msb = sum >> 8;
+	voltage_lsb = sum;
+
+	P1OUT  &= ~ (BIT0);    //  Select  Device
+	UCB0TXBUF = voltage_msb;
+	__delay_cycles(10);
+	UCB0TXBUF = voltage_lsb;
+	__delay_cycles(10);
+	P1OUT  |=   (BIT0); //  Unselect    Device
+
+	__delay_cycles(10);
+
+	sum = dac_B_status + current;
+
+	current_msb = sum >> 8;
+	current_lsb = sum;
+
+	P1OUT  &= ~ (BIT0);    //  Select  Device
+	UCB0TXBUF = current_msb;
+	__delay_cycles(10);
+	UCB0TXBUF = current_lsb;
+	__delay_cycles(10);
+	P1OUT  |=   (BIT0); //  Unselect    Device
+
+	__delay_cycles(10);
 }
 
 unsigned int measure_adc(void)
@@ -186,390 +145,57 @@ unsigned int measure_adc(void)
 	return raw_value;
 }
 
-int calculate_voltage(unsigned int adc_voltage_value)
+unsigned int measuring_data(void)
 {
-//	int Voltage = 0;
-	int Resistance = 0;
-	int Temperature = 0;
+	//Measure voltage
+	ADC10CTL0 = ADC10SHT_3 + ADC10ON; //64 clocks for sample
+	ADC10CTL1 = INCH_3 + ADC10DIV_3;	// Sensor is at channel X and CLK/4
+	__delay_cycles(3000);	// settle time 30ms
+	measured_voltage = measure_adc();
+	measured_voltage = (measured_voltage*250)/1023;
 
-//	Voltage = ((1023-adc_voltage_value)*3.6)/1023; // Convert 256-bit value to voltage
-	Resistance = ((10000*3.6/adc_voltage_value)); // Convert output voltage to resistance
-	Temperature = ResistanceToVoltage(Resistance);
-	return Temperature;
+	//Measure current
+	ADC10CTL0 = ADC10SHT_3 + ADC10ON; //64 clocks for sample
+	ADC10CTL1 = INCH_4 + ADC10DIV_3;	// Sensor is at channel X and CLK/4
+	__delay_cycles(3000);	// settle time 30ms
+	measured_current = measure_adc();
+	measured_current = (measured_current*250)/1023;
+
+	measured_resistance = measured_voltage / measured_current;
+	return measured_resistance;
 }
 
-int calculate_current(unsigned int adc_current_value)
+#pragma vector=USCIAB0RX_VECTOR		//interrupt service for recieving data
+__interrupt void USCI0RX_ISR(void)
 {
-//	int Voltage = 0;
-	int Resistance = 0;
-	int Temperature = 0;
+	data_recieved = UCA0RXBUF;
 
-//	Voltage = ((1023-adc_voltage_value)*3.6)/1023; // Convert 256-bit value to voltage
-	Resistance = ((10000*3.6/adc_current_value)); // Convert output voltage to resistance
-	Temperature = ResistanceToCurrent(Resistance);
-	return Temperature;
+	if(data_number==0)
+	{
+		recieved_voltage = UCA0RXBUF;
+		P1OUT ^= BIT6;
+		__delay_cycles(1000);		//avoid bouncing
+		P1OUT ^= BIT6;
+		data_number = 1;
+	}
+	else
+	{
+		recieved_current = UCA0RXBUF;
+		P1OUT ^= BIT6;
+		__delay_cycles(1000);		//avoid bouncing
+		P1OUT ^= BIT6;
+
+		calculated_resistance = recieved_voltage/recieved_current;
+		processed_current_data[calculated_resistance] = recieved_current;
+		if((recieved_voltage != 0)&&(recieved_current == 0))
+		{
+			processed_voltage_data = recieved_voltage;
+		}
+		data_number = 0;
+	}
 }
 
-signed int ResistanceToVoltage(int Resistance)
-{
-	signed int termo=0;
 
-	if(Resistance>31308)
-	{
-		termo=0;
-	}
-	else if ((Resistance<=31308)&&(Resistance>29749))
-	{
-		termo=1;
-	}
-	else if ((Resistance<=29749)&&(Resistance>28276))
-	{
-		termo=2;
-	}
-	else if ((Resistance<=28276)&&(Resistance>26885))
-	{
-		termo=3;
-	}
-	else if ((Resistance<=26885)&&(Resistance>25570))
-	{
-		termo=4;
-	}
-	else if ((Resistance<=25570)&&(Resistance>24327))
-	{
-		termo=5;
-	}
-	else if ((Resistance<=24327)&&(Resistance>23152))
-	{
-		termo=6;
-	}
-	else if ((Resistance<=23152)&&(Resistance>22141))
-	{
-		termo=7;
-	}
-	else if ((Resistance<=22141)&&(Resistance>20989))
-	{
-		termo=8;
-	}
-	else if ((Resistance<=20989)&&(Resistance>19993))
-	{
-		termo=9;
-	}
-	else if ((Resistance<=19993)&&(Resistance>19051))
-	{
-		termo=10;
-	}
-	else if ((Resistance<=19051)&&(Resistance>18158))
-	{
-		termo=11;
-	}
-	else if ((Resistance<=18158)&&(Resistance>17312))
-	{ //20-25C
-		termo=12;
-	}
-	else if ((Resistance<=17312)&&(Resistance>16511))
-	{ //25-30C
-		termo=13;
-	}
-	else if ((Resistance<=16511)&&(Resistance>15751))
-	{ //30-40C
-		termo=14;
-	}
-	else if ((Resistance<=15751)&&(Resistance>15031))
-	{ //40-50C
-		termo=15;
-	}
-	else if ((Resistance<=15031)&&(Resistance>14347))
-	{ //50-60C
-		termo=16;
-	}
-	else if ((Resistance<=14347)&&(Resistance>13698))
-	{ //60-70C
-		termo=17;
-	}
-	else if ((Resistance<=13698)&&(Resistance>13083))
-	{ //70-80C
-		termo=18;
-	}
-	else if ((Resistance<=13083)&&(Resistance>12499))
-	{ //80-90C
-		termo=19;
-	}
-	else if ((Resistance<=12499)&&(Resistance>11944))
-	{ //80-90C
-		termo=20; //I don't care if it's above 91 either
-	}
-	else if ((Resistance<=11944)&&(Resistance>11417))
-	{
-		termo=21;
-	}
-	else if ((Resistance<=11417)&&(Resistance>10916))
-	{ //20-25C
-		termo=22;
-	}
-	else if ((Resistance<=10916)&&(Resistance>10440))
-	{ //25-30C
-		termo=23;
-	}
-	else if ((Resistance<=10440)&&(Resistance>10000))
-	{ //30-40C
-		termo=24;
-	}
-	else if ((Resistance<=10000)&&(Resistance>9556))
-	{ //40-50C
-		termo=25;
-	}
-	else if ((Resistance<=9556)&&(Resistance>9147))
-	{ //50-60C
-		termo=26;
-	}
-	else if ((Resistance<=9147)&&(Resistance>8387))
-	{ //60-70C
-		termo=27;
-	}
-	else if ((Resistance<=8758)&&(Resistance>8387))
-	{ //70-80C
-		termo=28;
-	}
-	else if ((Resistance<=8387)&&(Resistance>8034))
-	{ //80-90C
-		termo=29;
-	}
-	else if ((Resistance<=8034)&&(Resistance>7700))
-	{ //80-90C
-		termo=30; //I don't care if it's above 91 either
-	}
-	else if ((Resistance<=7700)&&(Resistance>7377))
-	{
-		termo=31;
-	}
-	else if ((Resistance<=7377)&&(Resistance>7072))
-	{ //20-25C
-		termo=32;
-	}
-	else if ((Resistance<=7072)&&(Resistance>6781))
-	{ //25-30C
-		termo=33;
-	}
-	else if ((Resistance<=6781)&&(Resistance>6504))
-	{ //30-40C
-		termo=34;
-	}
-	else if ((Resistance<=6504)&&(Resistance>6239))
-	{ //40-50C
-		termo=35;
-	}
-	else if ((Resistance<=6239)&&(Resistance>5987))
-	{ //50-60C
-		termo=36;
-	}
-	else if ((Resistance<=5987)&&(Resistance>5746))
-	{ //60-70C
-		termo=37;
-	}
-	else if ((Resistance<=5746)&&(Resistance>5517))
-	{ //70-80C
-		termo=38;
-	}
-	else if ((Resistance<=5517)&&(Resistance>5298))
-	{ //80-90C
-		termo=39;
-	}
-	else if (Resistance<5298)
-	{ //80-90C
-		termo=40; //I don't care if it's above 91 either
-	}
-	 return termo;
-}
 
-signed int ResistanceToCurrent(int Resistance)
-{
-	signed int termo=0;
-
-	if(Resistance>31308)
-	{
-		termo=0;
-	}
-	else if ((Resistance<=31308)&&(Resistance>29749))
-	{
-		termo=1;
-	}
-	else if ((Resistance<=29749)&&(Resistance>28276))
-	{
-		termo=2;
-	}
-	else if ((Resistance<=28276)&&(Resistance>26885))
-	{
-		termo=3;
-	}
-	else if ((Resistance<=26885)&&(Resistance>25570))
-	{
-		termo=4;
-	}
-	else if ((Resistance<=25570)&&(Resistance>24327))
-	{
-		termo=5;
-	}
-	else if ((Resistance<=24327)&&(Resistance>23152))
-	{
-		termo=6;
-	}
-	else if ((Resistance<=23152)&&(Resistance>22141))
-	{
-		termo=7;
-	}
-	else if ((Resistance<=22141)&&(Resistance>20989))
-	{
-		termo=8;
-	}
-	else if ((Resistance<=20989)&&(Resistance>19993))
-	{
-		termo=9;
-	}
-	else if ((Resistance<=19993)&&(Resistance>19051))
-	{
-		termo=10;
-	}
-	else if ((Resistance<=19051)&&(Resistance>18158))
-	{
-		termo=11;
-	}
-	else if ((Resistance<=18158)&&(Resistance>17312))
-	{ //20-25C
-		termo=12;
-	}
-	else if ((Resistance<=17312)&&(Resistance>16511))
-	{ //25-30C
-		termo=13;
-	}
-	else if ((Resistance<=16511)&&(Resistance>15751))
-	{ //30-40C
-		termo=14;
-	}
-	else if ((Resistance<=15751)&&(Resistance>15031))
-	{ //40-50C
-		termo=15;
-	}
-	else if ((Resistance<=15031)&&(Resistance>14347))
-	{ //50-60C
-		termo=16;
-	}
-	else if ((Resistance<=14347)&&(Resistance>13698))
-	{ //60-70C
-		termo=17;
-	}
-	else if ((Resistance<=13698)&&(Resistance>13083))
-	{ //70-80C
-		termo=18;
-	}
-	else if ((Resistance<=13083)&&(Resistance>12499))
-	{ //80-90C
-		termo=19;
-	}
-	else if ((Resistance<=12499)&&(Resistance>11944))
-	{ //80-90C
-		termo=20; //I don't care if it's above 91 either
-	}
-	else if ((Resistance<=11944)&&(Resistance>11417))
-	{
-		termo=21;
-	}
-	else if ((Resistance<=11417)&&(Resistance>10916))
-	{ //20-25C
-		termo=22;
-	}
-	else if ((Resistance<=10916)&&(Resistance>10440))
-	{ //25-30C
-		termo=23;
-	}
-	else if ((Resistance<=10440)&&(Resistance>10000))
-	{ //30-40C
-		termo=24;
-	}
-	else if ((Resistance<=10000)&&(Resistance>9556))
-	{ //40-50C
-		termo=25;
-	}
-	else if ((Resistance<=9556)&&(Resistance>9147))
-	{ //50-60C
-		termo=26;
-	}
-	else if ((Resistance<=9147)&&(Resistance>8387))
-	{ //60-70C
-		termo=27;
-	}
-	else if ((Resistance<=8758)&&(Resistance>8387))
-	{ //70-80C
-		termo=28;
-	}
-	else if ((Resistance<=8387)&&(Resistance>8034))
-	{ //80-90C
-		termo=29;
-	}
-	else if ((Resistance<=8034)&&(Resistance>7700))
-	{ //80-90C
-		termo=30; //I don't care if it's above 91 either
-	}
-	else if ((Resistance<=7700)&&(Resistance>7377))
-	{
-		termo=31;
-	}
-	else if ((Resistance<=7377)&&(Resistance>7072))
-	{ //20-25C
-		termo=32;
-	}
-	else if ((Resistance<=7072)&&(Resistance>6781))
-	{ //25-30C
-		termo=33;
-	}
-	else if ((Resistance<=6781)&&(Resistance>6504))
-	{ //30-40C
-		termo=34;
-	}
-	else if ((Resistance<=6504)&&(Resistance>6239))
-	{ //40-50C
-		termo=35;
-	}
-	else if ((Resistance<=6239)&&(Resistance>5987))
-	{ //50-60C
-		termo=36;
-	}
-	else if ((Resistance<=5987)&&(Resistance>5746))
-	{ //60-70C
-		termo=37;
-	}
-	else if ((Resistance<=5746)&&(Resistance>5517))
-	{ //70-80C
-		termo=38;
-	}
-	else if ((Resistance<=5517)&&(Resistance>5298))
-	{ //80-90C
-		termo=39;
-	}
-	else if (Resistance<5298)
-	{ //80-90C
-		termo=40; //I don't care if it's above 91 either
-	}
-	 return termo;
-}
-
-//
-//void send_systemdata(int data)
-//{
-//	while (!(IFG2&UCA0TXIFG));  // Wait until TX buffer is ready
-//	UCA0TXBUF = data;
-//}
-
-#pragma vector=TIMER0_A0_VECTOR
-__interrupt void Timer_A (void)
-{
-	counter = counter + 1;
-//	send_systemdata((int) (counter));
-	if((counter)%2 == 0)
-	{
-		P1OUT ^= LED_0;
-		send_data = 1;
-		counter = 0;
-	}
-}
 
 
